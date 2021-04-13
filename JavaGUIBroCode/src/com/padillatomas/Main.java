92629837fac995f6a6a582a80614cb082c3e8c63
Tomas Padilla <padillatomasagustin@gmail.com>
2021-04-13 08:16:05 +0200

    First Push Java GUI

---
+++ JavaGUIBroCode/src/com/padillatomas/Main.java
@@ -0,0 +1,22 @@
+package com.padillatomas;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		
+//		Window:
+		frame.setSize(420,420);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Hola Katy");
+		
+//		Main:
+		frame.setVisible(true);
+		
+		
+
+	}
+
+}
